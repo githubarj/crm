109d22bf758c540e3cd91926d6594a0a980fb3d6
Richard Githuba <githuba9520@gmail.com>
2024-04-03 17:03:22 +0300

    header icon fix

--- src/Components/App/AppHeader.jsx
+++ src/Components/App/AppHeader.jsx
@@ -1,6 +1,7 @@
 import { SearchOutlined, UserOutlined } from "@ant-design/icons";
 import { Layout, Button, Row, Col, Badge, Avatar } from "antd";
 import { IoMoonOutline } from "react-icons/io5";
+import { GoBell } from "react-icons/go";
 const AppHeader = () => {
 	const { Header } = Layout;
 	return (
@@ -11,7 +12,7 @@ const AppHeader = () => {
 				</Col>
 				<Col className="header-btns">
 					<Button shape="circle" type="text" icon={<IoMoonOutline />} />
-					<Button shape="circle" type="text" icon={<SearchOutlined />} />
+					<Button shape="circle" type="text" icon={<GoBell />} />
 					<Badge dot>
 						<Avatar shape="circle" icon={<UserOutlined />} />
 					</Badge>
