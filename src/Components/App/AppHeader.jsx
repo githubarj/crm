8044149d24a1bf89bef8735a813c28725f2310f0
Richard Githuba <githuba9520@gmail.com>
2024-04-03 17:54:34 +0300

    dropdown for the avatar 90% done

--- src/Components/App/AppHeader.jsx
+++ src/Components/App/AppHeader.jsx
@@ -7,6 +7,8 @@ import {
 	Badge,
 	Avatar,
 	Dropdown,
+	Flex,
+	Typography,
 } from "antd";
 import { IoMoonOutline } from "react-icons/io5";
 import { GoBell } from "react-icons/go";
@@ -18,10 +20,29 @@ import {
 	CiCircleQuestion,
 } from "react-icons/ci";
 import { LiaDollarSignSolid } from "react-icons/lia";
+import { IoIosLogOut } from "react-icons/io";
 
 const AppHeader = () => {
 	const { Header } = Layout;
+	const { Paragraph, Title, Text } = Typography;
 	const items = [
+		{
+			label: (
+				<Flex gap={10} align="center">
+					<Avatar shape="circle" icon={<UserOutlined />} />
+					<Paragraph style={{ marginBottom: "0" }}>
+						<Title level={5} style={{ marginBottom: "0" }}>
+							John Doe
+						</Title>
+						<Text>Admin</Text>
+					</Paragraph>
+				</Flex>
+			),
+			key: "0",
+		},
+		{
+			type: "divider",
+		},
 		{
 			label: "Profile",
 			key: "1",
@@ -38,6 +59,9 @@ const AppHeader = () => {
 			icon: <CiChat1 />,
 		},
 		{
+			type: "divider",
+		},
+		{
 			label: "Settings",
 			key: "4",
 			icon: <CiSettings />,
@@ -51,6 +75,14 @@ const AppHeader = () => {
 			label: "Faqs",
 			key: "6",
 			icon: <CiCircleQuestion />,
+		},
+		{
+			type: "divider",
+		},
+		{
+			label: "Logout",
+			key: "8",
+			icon: <IoIosLogOut />,
 		},
 	];
 	const menuProps = {
