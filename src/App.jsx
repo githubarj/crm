3309ed4c86f5e6a74dea6e66c8e47ed275b22f59
Richard Githuba <githuba9520@gmail.com>
2024-04-03 16:31:00 +0300

    knowledge-base header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,44 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import Authentication from "./Routes/Layouts/Authentication"
-import LogIn from "./Routes/Pages/Auth/LogIn"
-import Register from "./Routes/Pages/Auth/Register"
-import ForgotPassword from "./Routes/Pages/Auth/ForgotPassword"
-import ResetPassword from "./Routes/Pages/Auth/ResetPassword"
-import AppLayout from "./Routes/Layouts/AppLayout"
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Authentication from "./Routes/Layouts/Authentication";
+import LogIn from "./Routes/Pages/Auth/LogIn";
+import Register from "./Routes/Pages/Auth/Register";
+import ForgotPassword from "./Routes/Pages/Auth/ForgotPassword";
+import ResetPassword from "./Routes/Pages/Auth/ResetPassword";
+import AppLayout from "./Routes/Layouts/AppLayout";
+import KnowledgeBase from "./Routes/Pages/Home/KnowledgeBase";
 
 const routes = createBrowserRouter([
-  {
-    path : "/",
-    element: <Authentication />,
-    children: [
-      
-          {
-            index: true,
-            element: <LogIn />
-          },
-          {
-            path: "register",
-            element: <Register />
-          },
-          {
-            path: "forgot",
-            element: <ForgotPassword />
-          },
-          {
-            path: "reset",
-            element: <ResetPassword />
-          }
-        ]
-  },
-  {
-    path: "/home",
-    element: <AppLayout/>
-  }
-])
-
+	{
+		path: "/",
+		element: <Authentication />,
+		children: [
+			{
+				index: true,
+				element: <LogIn />,
+			},
+			{
+				path: "register",
+				element: <Register />,
+			},
+			{
+				path: "forgot",
+				element: <ForgotPassword />,
+			},
+			{
+				path: "reset",
+				element: <ResetPassword />,
+			},
+		],
+	},
+	{
+		path: "/home",
+		element: <AppLayout />,
+		children: [{ index: true, element: <KnowledgeBase /> }],
+	},
+]);
 
 function App() {
-  return (
-    <RouterProvider router={routes}/>
-  )
+	return <RouterProvider router={routes} />;
 }
 
-export default App
+export default App;
---
+++ src/Routes/Pages/Home/KnowledgeBase.jsx
@@ -0,0 +1,33 @@
+import { SearchOutlined } from "@ant-design/icons";
+import { Flex, Input, Row, Typography } from "antd";
+
+function KnowledgeBase() {
+	const { Title, Text } = Typography;
+	return (
+		<Flex vertical gap={10} className="knowledge-base">
+			<Flex vertical align="center" gap={10} className="knowledge-base__header">
+				<Flex vertical align="center" className="header-text" >
+					<Title level={3} style={{ color: "blue" }}>
+						Hello, how can we help ?
+					</Title>
+					<Text>or choose a category to quickly find the help you need</Text>
+				</Flex>
+				<Input
+					prefix={<SearchOutlined />}
+					placeholder="Ask a question..."
+					className="header-input"
+				/>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+					<path
+						fill="#E4E8F6"
+						fillOpacity="1"
+						d="M0,32L1440,320L1440,320L0,320Z"
+					></path>
+				</svg>
+			</Flex>
+			<Row></Row>
+		</Flex>
+	);
+}
+
+export default KnowledgeBase;
